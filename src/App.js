56cde5a92901567395af4e76a1bf93dc15739c57
Your Name <you@example.com>
2021-03-08 11:35:28 +0100

    Initial

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import Title from './Components/Title'
+
+function App() {
+  return (
+
+    <div>
+      <div className="app">
+
+        <Title texto='Mi primera React App'></Title>
+        
+        <h3>Hola Mundo</h3>
+      </div>
+
+      <div>
+        <p>¿Cómo están?</p>
+      </div>
+    </div>
+
+  );
+}
+
+export default App;
